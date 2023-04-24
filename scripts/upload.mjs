a87021cf630613a7c79588d11cdc61f5f27880e8
Ribbit <echo07200503@gmail.com>
2023-04-24 14:10:30 +0900

    ✨ スクリプトファイルを最新化

---
+++ scripts/upload.mjs
@@ -0,0 +1,26 @@
+//@ts-check
+import { exec } from 'child_process';
+import { config } from 'dotenv';
+config();
+
+const { KINTONE_BASE_URL, KINTONE_USERNAME, KINTONE_PASSWORD } = process.env;
+if (!KINTONE_BASE_URL || !KINTONE_USERNAME || !KINTONE_PASSWORD) {
+  throw `.envの設定が不十分です。以下のパラメータは必須です
+    KINTONE_BASE_URL
+    KINTONE_USERNAME
+    KINTONE_PASSWORD
+    `;
+}
+
+exec(
+  `kintone-plugin-uploader dist/plugin-dev.zip --base-url ${KINTONE_BASE_URL} --username ${KINTONE_USERNAME} --password ${KINTONE_PASSWORD} --watch --waiting-dialog-ms 3000`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  }
+);
+console.log('🐢 プラグインをアップロードしました');
