5d89eefc3cf407b372e0ac8a3f47441f9457ecc4
Ribbit <echo07200503@gmail.com>
2023-09-22 23:12:17 +0900

    🚀 1.10.0

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -8,7 +8,7 @@ export default {
   manifest: {
     base: {
       manifest_version: 1,
-      version: '1.9.2',
+      version: '1.10.0',
       type: 'APP',
       name: {
         en: 'unrelated lookup plugin',
