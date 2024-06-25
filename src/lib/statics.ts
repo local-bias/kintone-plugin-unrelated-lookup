48691e2b5fd4a8e3673bc7342d5b3ccca03ea84a
Ribbit <echo07200503@gmail.com>
2024-06-25 15:46:58 +0900

    ♻ プラグイン設定情報の参照先を修正

--- src/lib/statics.ts
+++ src/lib/statics.ts
@@ -1,6 +1,6 @@
-import manifest from 'plugin/manifest.json';
+import config from 'plugin.config.mjs';
 
-export const PLUGIN_NAME = manifest.name.ja;
+export const PLUGIN_NAME = config.manifest.base.name.ja;
 
 export const URL_INQUIRY = 'https://form.konomi.app';
 export const URL_PROMOTION = 'https://promotion.konomi.app/kintone-plugin';
