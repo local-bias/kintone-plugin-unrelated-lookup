b75195921b3ff061e3f6632b892dff50cd39ee9a
Ribbit <echo07200503@gmail.com>
2024-09-14 08:33:03 +0900

    💄 プラグイン設定画面ではpreflightを利用

--- tailwind.config.desktop.mjs
+++ tailwind.config.desktop.mjs
@@ -4,5 +4,8 @@ import common from './tailwind.config.common.mjs';
 /** @type { import('tailwindcss').Config } */
 export default {
   ...common,
+  corePlugins: {
+    preflight: false,
+  },
   content: ['./src/desktop/**/*.{ts,js,jsx,tsx}'],
 };
