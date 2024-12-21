994ba43c98e1f86eab40ca9da9e5bdeac16afff8
Ribbit <echo07200503@gmail.com>
2024-12-21 10:55:13 +0900

    ✨ フォームに並び替え条件と動的絞り込みセクションを追加し、フェールソフトモードの設定を実装

--- src/config/components/model/form/index.tsx
+++ src/config/components/model/form/index.tsx
@@ -11,6 +11,7 @@ import {
   conditionTypeState,
   enablesValidationState,
   isCaseSensitiveState,
+  isFailSoftEnabledState,
   isHankakuKatakanaSensitiveState,
   isKatakanaSensitiveState,
   isZenkakuEisujiSensitiveState,
@@ -26,6 +27,7 @@ import DstSubtableForm from './form-dst-subtable';
 import DynamicConditionsForm from './form-dynamic-conditions';
 import SrcAppForm from './form-src-app';
 import SrcFieldForm from './form-src-field';
+import SortCriteriaForm from './form-sort-criteria';
 
 const Component: FC = () => (
   <div className='p-4'>
@@ -64,7 +66,7 @@ const Component: FC = () => (
     </PluginFormSection>
 
     <PluginFormSection>
-      <PluginFormTitle>関連付けないアプリ(参照先アプリ)</PluginFormTitle>
+      <PluginFormTitle>参照先アプリ</PluginFormTitle>
       <PluginFormDescription last>
         標準機能の「関連付けるアプリ」にあたる、ルックアップの取得先アプリを選択してください。
       </PluginFormDescription>
@@ -104,14 +106,6 @@ const Component: FC = () => (
     </PluginFormSection>
 
     <PluginFormSection>
-      <PluginFormTitle>対象データの動的絞り込み</PluginFormTitle>
-      <PluginFormDescription last>
-        参照するデータを、編集しているレコードの特定のフィールドの値によって動的に絞り込むことができます。
-      </PluginFormDescription>
-      <DynamicConditionsForm />
-    </PluginFormSection>
-
-    <PluginFormSection>
       <PluginFormTitle>コピー元レコードの取得条件</PluginFormTitle>
       <PluginFormDescription>
         ルックアップ実行時に、対象レコードを絞り込む条件を設定します。
@@ -128,34 +122,74 @@ const Component: FC = () => (
     </PluginFormSection>
 
     <PluginFormSection>
+      <PluginFormTitle>対象データの動的絞り込み</PluginFormTitle>
+      <PluginFormDescription last>
+        参照するデータを、編集しているレコードの特定のフィールドの値によって動的に絞り込むことができます。
+      </PluginFormDescription>
+      <DynamicConditionsForm />
+    </PluginFormSection>
+
+    <PluginFormSection>
+      <PluginFormTitle>並び替え条件</PluginFormTitle>
+      <PluginFormDescription last>
+        対象レコードを選択するダイアログ上で表示するレコードの並び順を設定します。
+      </PluginFormDescription>
+      <SortCriteriaForm />
+    </PluginFormSection>
+
+    <PluginFormSection>
       <PluginFormTitle>その他のオプション</PluginFormTitle>
       <PluginFormDescription last></PluginFormDescription>
       <div className='flex flex-col gap-1'>
-        <RecoilSwitch
-          label='コピー先に標準のルックアップフィールドが存在する場合、取得完了後自動的にルックアップを実行する'
-          state={autoLookupState}
-        />
-        <RecoilSwitch
-          label='レコード保存時に、ルックアップが実行されていない場合はエラーを表示する'
-          state={enablesValidationState}
-        />
-        <RecoilSwitch label='レコード保存時に、ルックアップを実行する' state={saveAndLookupState} />
-        <RecoilSwitch
-          label='絞り込みの際、アルファベットの大文字と小文字を区別する'
-          state={isCaseSensitiveState}
-        />
-        <RecoilSwitch
-          label='絞り込みの際、カタカナとひらがなを区別する'
-          state={isKatakanaSensitiveState}
-        />
-        <RecoilSwitch
-          label='絞り込みの際、半角カナと全角カナを区別する'
-          state={isHankakuKatakanaSensitiveState}
-        />
-        <RecoilSwitch
-          label='絞り込みの際、全角英数字と半角英数字を区別する'
-          state={isZenkakuEisujiSensitiveState}
-        />
+        <div className='px-4 py-2 ml-4 mt-2 border-l'>
+          <h3 className='text-base font-bold'>標準機能との連携</h3>
+          <RecoilSwitch
+            label='コピー先に標準のルックアップフィールドが存在する場合、取得完了後自動的にルックアップを実行する'
+            state={autoLookupState}
+          />
+        </div>
+        <div className='px-4 py-2 ml-4 mt-2 border-l'>
+          <h3 className='text-base font-bold'>レコード保存時のふるまい</h3>
+          <div className='grid gap-1'>
+            <RecoilSwitch
+              label='レコード保存時に、ルックアップが実行されていない場合はエラーを表示する'
+              state={enablesValidationState}
+            />
+            <RecoilSwitch
+              label='レコード保存時に、ルックアップを実行する'
+              state={saveAndLookupState}
+            />
+          </div>
+        </div>
+
+        <div className='px-4 py-2 ml-4 mt-2 border-l'>
+          <h3 className='text-base font-bold'>あいまい検索の設定</h3>
+          <div className='grid gap-1'>
+            <RecoilSwitch
+              label='絞り込みの際、アルファベットの大文字と小文字を区別する'
+              state={isCaseSensitiveState}
+            />
+            <RecoilSwitch
+              label='絞り込みの際、カタカナとひらがなを区別する'
+              state={isKatakanaSensitiveState}
+            />
+            <RecoilSwitch
+              label='絞り込みの際、半角カナと全角カナを区別する'
+              state={isHankakuKatakanaSensitiveState}
+            />
+            <RecoilSwitch
+              label='絞り込みの際、全角英数字と半角英数字を区別する'
+              state={isZenkakuEisujiSensitiveState}
+            />
+          </div>
+        </div>
+        <div className='px-4 py-2 ml-4 mt-2 border-l'>
+          <h3 className='text-base font-bold'>フェールソフトモード</h3>
+          <PluginFormDescription>
+            フェールソフトモードを有効にすると、プラグイン設定に誤りがありレコードの取得が正常に行えない場合でも、全件取得を行うことでエラーを回避します。
+          </PluginFormDescription>
+          <RecoilSwitch label='フェールソフトモードを有効にする' state={isFailSoftEnabledState} />
+        </div>
       </div>
     </PluginFormSection>
     <DeleteButton />
