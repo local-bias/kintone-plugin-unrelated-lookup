91b472de784daaf6a1f7bb6d5e1e4dc6559b84aa
Ribbit <echo07200503@gmail.com>
2024-12-21 09:05:39 +0900

    ✨ レコードのソートとハンドリング機能を追加し、フィルタ条件に基づく処理を改善

--- src/desktop/embedding-lookup-button/states/records.ts
+++ src/desktop/embedding-lookup-button/states/records.ts
@@ -1,5 +1,10 @@
 import { isProd } from '@/lib/global';
-import { getYuruChara, kintoneAPI } from '@konomi-app/kintone-utilities';
+import {
+  getFieldValueAsString,
+  getYuruChara,
+  kintoneAPI,
+  sortField,
+} from '@konomi-app/kintone-utilities';
 import { atom } from 'jotai';
 import { atomFamily } from 'jotai/utils';
 import { areAttachmentsEqual, AttachmentAtomParams, pluginConditionAtom, searchInputAtom } from '.';
@@ -7,14 +12,72 @@ import { dialogPageChunkAtom, dialogPageIndexAtom } from './dialog';
 
 export type HandledRecord = { __quickSearch: string; record: kintoneAPI.RecordData };
 
-export const srcAllRecordsAtom = atomFamily((_conditionId: string) => atom<HandledRecord[]>([]));
+export const srcAllRawRecordsAtom = atomFamily((_conditionId: string) =>
+  atom<kintoneAPI.RecordData[]>([])
+);
+
+const srcAllSortedRecordsAtom = atomFamily((conditionId: string) =>
+  atom<kintoneAPI.RecordData[]>((get) => {
+    const condition = get(pluginConditionAtom(conditionId));
+    const records = get(srcAllRawRecordsAtom(conditionId));
+
+    const sortCriteria = condition.sortCriteria.filter(({ fieldCode }) => !!fieldCode);
+
+    return records.toSorted((a, b) => {
+      for (const { fieldCode, order } of sortCriteria) {
+        const aField = a[fieldCode];
+        const bField = b[fieldCode];
+
+        if (!aField || !bField) {
+          continue;
+        }
+
+        const sortResult = sortField(aField, bField);
+        if (sortResult === 0) {
+          continue;
+        }
+        return order === 'asc' ? sortResult : -sortResult;
+      }
+      return 0;
+    });
+  })
+);
+
+export const srcAllHandledRecordsAtom = atomFamily((conditionId: string) =>
+  atom<HandledRecord[]>((get) => {
+    const condition = get(pluginConditionAtom(conditionId));
+    const rawRecords = get(srcAllSortedRecordsAtom(conditionId));
+
+    const {
+      isCaseSensitive,
+      isKatakanaSensitive,
+      isZenkakuEisujiSensitive,
+      isHankakuKatakanaSensitive,
+    } = condition;
+
+    return rawRecords.map((record) => {
+      let __quickSearch = Object.values(record)
+        .map((field) => getFieldValueAsString(field))
+        .join('__');
+
+      __quickSearch = getYuruChara(__quickSearch, {
+        isCaseSensitive,
+        isKatakanaSensitive,
+        isZenkakuEisujiSensitive,
+        isHankakuKatakanaSensitive,
+      });
+
+      return { record, __quickSearch };
+    });
+  })
+);
 
 export const filteredRecordsAtom = atomFamily(
   (params: AttachmentAtomParams) =>
     atom<kintoneAPI.RecordData[]>((get) => {
       const { conditionId } = params;
       const condition = get(pluginConditionAtom(conditionId));
-      const records = get(srcAllRecordsAtom(conditionId));
+      const records = get(srcAllHandledRecordsAtom(conditionId));
       const text = get(searchInputAtom(params));
 
       const {
