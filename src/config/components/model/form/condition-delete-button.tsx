6c2ea170f5e02bd3cc27e9633720da066fad228c
Ribbit <echo07200503@gmail.com>
2025-01-25 14:21:11 +0900

    ♻ recoil -> jotai

--- src/config/components/model/form/condition-delete-button.tsx
+++ src/config/components/model/form/condition-delete-button.tsx
@@ -1,27 +1,26 @@
 import { PluginConditionDeleteButton } from '@konomi-app/kintone-utilities-react';
 import { produce } from 'immer';
+import { useAtomValue } from 'jotai';
+import { RESET, useAtomCallback } from 'jotai/utils';
 import { useSnackbar } from 'notistack';
-import { FC, memo } from 'react';
-import { useRecoilCallback, useRecoilValue } from 'recoil';
-import { selectedConditionIdState, storageState } from '../../../states/plugin';
+import { FC, memo, useCallback } from 'react';
+import { pluginConfigAtom, selectedConditionIdAtom } from '../../../states/plugin';
 
 const Container: FC = () => {
   const { enqueueSnackbar } = useSnackbar();
-  const storage = useRecoilValue(storageState);
+  const storage = useAtomValue(pluginConfigAtom);
 
-  const onClick = useRecoilCallback(
-    ({ snapshot, set, reset }) =>
-      async () => {
-        const selectedConditionId = await snapshot.getPromise(selectedConditionIdState);
-        set(storageState, (_, _storage = _!) =>
-          produce(_storage, (draft) => {
-            draft.conditions = draft.conditions.filter((c) => c.id !== selectedConditionId);
-          })
-        );
-        reset(selectedConditionIdState);
-        enqueueSnackbar('設定を削除しました', { variant: 'success' });
-      },
-    []
+  const onClick = useAtomCallback(
+    useCallback(async (get, set) => {
+      const selectedConditionId = await get(selectedConditionIdAtom);
+      set(pluginConfigAtom, (_, _storage = _!) =>
+        produce(_storage, (draft) => {
+          draft.conditions = draft.conditions.filter((c) => c.id !== selectedConditionId);
+        })
+      );
+      set(selectedConditionIdAtom, RESET);
+      enqueueSnackbar('設定を削除しました', { variant: 'success' });
+    }, [])
   );
 
   if ((storage?.conditions.length ?? 0) < 2) {
