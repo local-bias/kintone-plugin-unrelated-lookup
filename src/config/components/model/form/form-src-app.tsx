5914465a49c2535f2555295ab30de733d876331b
Ribbit <echo07200503@gmail.com>
2025-01-25 14:21:24 +0900

    ♻ recoil -> jotai

--- src/config/components/model/form/form-src-app.tsx
+++ src/config/components/model/form/form-src-app.tsx
@@ -1,33 +1,32 @@
-import { Autocomplete, Skeleton, TextField } from '@mui/material';
-import { FC, FCX, memo, Suspense } from 'react';
-import { useRecoilCallback, useRecoilValue } from 'recoil';
-import { kintoneAppsState, kintoneSpacesState } from '../../../states/kintone';
-import { isSrcAppGuestSpaceState, srcAppIdState, srcSpaceIdState } from '../../../states/plugin';
+import { Autocomplete, Box, Skeleton, TextField } from '@mui/material';
+import { useAtomValue } from 'jotai';
+import { useAtomCallback } from 'jotai/utils';
+import { FC, FCX, memo, Suspense, useCallback } from 'react';
+import { kintoneAppsAtom, kintoneSpacesAtom } from '../../../states/kintone';
+import { isSrcAppGuestSpaceAtom, srcAppIdAtom, srcSpaceIdAtom } from '../../../states/plugin';
 
 const Component: FCX = () => {
-  const allApps = useRecoilValue(kintoneAppsState);
-  const srcAppId = useRecoilValue(srcAppIdState);
+  const allApps = useAtomValue(kintoneAppsAtom);
+  const srcAppId = useAtomValue(srcAppIdAtom);
 
-  const onAppChange = useRecoilCallback(
-    ({ snapshot, set }) =>
-      async (value: string) => {
-        set(srcAppIdState, value);
+  const onAppChange = useAtomCallback(
+    useCallback(async (get, set, value: string) => {
+      set(srcAppIdAtom, value);
 
-        const allApps = await snapshot.getPromise(kintoneAppsState);
-        const srcApp = allApps.find((app) => app.appId === value);
-        if (!srcApp) {
-          return;
-        }
+      const allApps = await get(kintoneAppsAtom);
+      const srcApp = allApps.find((app) => app.appId === value);
+      if (!srcApp) {
+        return;
+      }
 
-        const spaces = await snapshot.getPromise(kintoneSpacesState);
-        const srcSpace = spaces.find((space) => space.id === srcApp.spaceId);
-        if (!srcSpace) {
-          return;
-        }
-        set(srcSpaceIdState, srcSpace.id ?? null);
-        set(isSrcAppGuestSpaceState, srcSpace.isGuest);
-      },
-    []
+      const spaces = await get(kintoneSpacesAtom);
+      const srcSpace = spaces.find((space) => space.id === srcApp.spaceId);
+      if (!srcSpace) {
+        return;
+      }
+      set(srcSpaceIdAtom, srcSpace.id ?? null);
+      set(isSrcAppGuestSpaceAtom, srcSpace.isGuest);
+    }, [])
   );
 
   return (
@@ -38,6 +37,20 @@ const Component: FCX = () => {
       isOptionEqualToValue={(option, v) => option.appId === v.appId}
       getOptionLabel={(app) => `${app.name}(id: ${app.appId})`}
       onChange={(_, app) => onAppChange(app?.appId ?? '')}
+      renderOption={(props, app) => {
+        const { key, ...optionProps } = props;
+        return (
+          <Box key={key} component='li' {...optionProps}>
+            <div className='grid'>
+              <div className='text-xs text-gray-400'>
+                id: {app.code}
+                {app.appId}
+              </div>
+              {app.name}
+            </div>
+          </Box>
+        );
+      }}
       renderInput={(params) => (
         <TextField {...params} label='対象アプリ' variant='outlined' color='primary' />
       )}
