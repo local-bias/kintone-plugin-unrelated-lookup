ba43d4e27e2e29ef1015bd8b60c849d9e7588ce3
Ribbit <echo07200503@gmail.com>
2025-01-25 14:24:50 +0900

    ♻ 不要なインポートを削除

--- src/config/components/model/form/form-copies/index.tsx
+++ src/config/components/model/form/form-copies/index.tsx
@@ -1,6 +1,4 @@
-import { conditionTypeState } from '@/config/states/plugin';
 import { type FC } from 'react';
-import { useRecoilValue } from 'recoil';
 import Single from './single';
 
 const Component: FC = () => {
