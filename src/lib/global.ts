e748e0c10e7812d8dd251b0556d4ca352dbdf854
Ribbit <echo07200503@gmail.com>
2025-01-25 14:26:09 +0900

    ♻ LANGUAGEの取得方法を修正

--- src/lib/global.ts
+++ src/lib/global.ts
@@ -2,7 +2,7 @@ import { detectGuestSpaceId } from '@konomi-app/kintone-utilities';
 
 export const PLUGIN_ID = kintone.$PLUGIN_ID;
 export const GUEST_SPACE_ID = detectGuestSpaceId() ?? undefined;
-export const LANGUAGE = kintone.getLoginUser().language as 'ja' | 'en' | 'zh' | 'es';
+export const LANGUAGE = kintone.getLoginUser()?.language;
 
 export const ENV = (process?.env?.NODE_ENV ?? 'production') as 'production' | 'development';
 export const isProd = ENV === 'production';
