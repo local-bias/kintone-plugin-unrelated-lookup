eb3dc498ccda6c6b263f08d4d1800b046ab5aec9
Ribbit <echo07200503@gmail.com>
2025-01-25 14:21:39 +0900

    ♻ recoil -> jotai

--- src/config/components/model/form/select-src-fields.tsx
+++ src/config/components/model/form/select-src-fields.tsx
@@ -1,6 +1,6 @@
+import { JotaiFieldSelect } from '@konomi-app/kintone-utilities-jotai';
 import { FC } from 'react';
 import { srcAppPropertiesState } from '../../../states/kintone';
-import { RecoilFieldSelect } from '@konomi-app/kintone-utilities-recoil';
 
 type Props = {
   fieldCode: string;
@@ -10,7 +10,9 @@ type Props = {
 };
 
 const Component: FC<Props> = (props) => {
-  return <RecoilFieldSelect state={srcAppPropertiesState} {...props} />;
+  return (
+    <JotaiFieldSelect sx={{ width: 320 }} fieldPropertiesAtom={srcAppPropertiesState} {...props} />
+  );
 };
 
 export default Component;
