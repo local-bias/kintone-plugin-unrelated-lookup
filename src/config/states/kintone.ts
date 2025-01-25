bb96b4632de84453013a140d66032e7e0e04d079
Ribbit <echo07200503@gmail.com>
2025-01-25 14:23:48 +0900

    ♻ recoil -> jotai

--- src/config/states/kintone.ts
+++ src/config/states/kintone.ts
@@ -1,4 +1,4 @@
-import { ENV, GUEST_SPACE_ID } from '@/lib/global';
+import { GUEST_SPACE_ID, isProd } from '@/lib/global';
 import { FIELD_TYPES_SYSTEM, omitFieldProperties } from '@/lib/kintone-api';
 import {
   filterFieldProperties,
@@ -9,26 +9,25 @@ import {
   withSpaceIdFallback,
 } from '@konomi-app/kintone-utilities';
 import { kintoneAPI } from '@konomi-app/kintone-utilities/dist/types/api';
-import { selector } from 'recoil';
-import { dstSubtableFieldCodeState, srcAppIdState } from './plugin';
+import { atom } from 'jotai';
+import { dstSubtableFieldCodeAtom, srcAppIdAtom } from './plugin';
 
-const PREFIX = 'kintone';
-
-export const kintoneAppsState = selector({
-  key: `${PREFIX}kintoneAppsState`,
-  get: async ({ get }) => {
-    const apps = await getAllApps({
-      guestSpaceId: GUEST_SPACE_ID,
-      debug: ENV === 'development',
-    });
-    return apps;
-  },
+const currentAppIdAtom = atom<number>(() => {
+  const appId = getAppId();
+  if (!appId) {
+    throw new Error('アプリIDが取得できませんでした');
+  }
+  return appId;
 });
 
-export const kintoneSpacesState = selector<kintoneAPI.rest.space.GetSpaceResponse[]>({
-  key: `${PREFIX}kintoneSpacesState`,
-  get: async ({ get }) => {
-    const apps = get(kintoneAppsState);
+export const kintoneAppsAtom = atom(async () => {
+  const apps = await getAllApps({ guestSpaceId: GUEST_SPACE_ID, debug: !isProd });
+  return apps;
+});
+
+export const kintoneSpacesAtom = atom<Promise<kintoneAPI.rest.space.GetSpaceResponse[]>>(
+  async (get) => {
+    const apps = await get(kintoneAppsAtom);
     const spaceIds = [
       ...new Set(apps.filter((app) => app.spaceId).map<string>((app) => app.spaceId as string)),
     ];
@@ -38,57 +37,42 @@ export const kintoneSpacesState = selector<kintoneAPI.rest.space.GetSpaceRespons
       const space = await withSpaceIdFallback({
         spaceId: id,
         func: getSpace,
-        funcParams: { id, debug: true },
+        funcParams: { id, debug: !isProd },
       });
       spaces.push(space);
     }
 
     return spaces;
-  },
+  }
+);
+
+export const appFieldsState = atom<Promise<kintoneAPI.FieldProperties>>(async (get) => {
+  const app = get(currentAppIdAtom);
+  const { properties } = await getFormFields({
+    app,
+    preview: true,
+    guestSpaceId: GUEST_SPACE_ID,
+    debug: !isProd,
+  });
+  const omitted = omitFieldProperties(properties, [...FIELD_TYPES_SYSTEM, 'SUBTABLE']);
+
+  return omitted;
 });
 
-export const appFieldsState = selector<kintoneAPI.FieldProperties>({
-  key: `${PREFIX}AppFields`,
-  get: async ({ get }) => {
-    const app = getAppId();
-    if (!app) {
-      throw new Error('アプリのフィールド情報が取得できませんでした');
-    }
-    const { properties } = await getFormFields({
-      app,
-      preview: true,
-      guestSpaceId: GUEST_SPACE_ID,
-      debug: ENV === 'development',
-    });
-    const omitted = omitFieldProperties(properties, [...FIELD_TYPES_SYSTEM, 'SUBTABLE']);
-
-    return omitted;
-  },
+export const dstAppPropertiesState = atom<Promise<kintoneAPI.FieldProperties>>(async (get) => {
+  const app = get(currentAppIdAtom);
+  const { properties } = await getFormFields({
+    app,
+    preview: true,
+    guestSpaceId: GUEST_SPACE_ID,
+    debug: !isProd,
+  });
+  return properties;
 });
 
-export const dstAppPropertiesState = selector<kintoneAPI.FieldProperties>({
-  key: `${PREFIX}dstAppPropertiesState`,
-  get: async ({ get }) => {
-    const app = getAppId();
-    if (!app) {
-      throw new Error('アプリのフィールド情報が取得できませんでした');
-    }
-
-    const { properties } = await getFormFields({
-      app,
-      preview: true,
-      guestSpaceId: GUEST_SPACE_ID,
-      debug: ENV === 'development',
-    });
-
-    return properties;
-  },
-});
-
-export const dstAppDynamicFilterPropertiesState = selector<kintoneAPI.FieldProperty[]>({
-  key: `${PREFIX}dstAppDynamicFilterPropertiesState`,
-  get: async ({ get }) => {
-    const dstAppProperties = get(dstAppPropertiesState);
+export const dstAppDynamicFilterPropertiesState = atom<Promise<kintoneAPI.FieldProperty[]>>(
+  async (get) => {
+    const dstAppProperties = await get(dstAppPropertiesState);
     const filtered = filterFieldProperties(
       dstAppProperties,
       (field) =>
@@ -102,98 +86,68 @@ export const dstAppDynamicFilterPropertiesState = selector<kintoneAPI.FieldPrope
         field.type === 'STATUS_ASSIGNEE'
     );
     return Object.values(filtered).sort((a, b) => a.label.localeCompare(b.label, 'ja'));
-  },
-});
+  }
+);
 
-export const copyableDstAppPropertiesState = selector<kintoneAPI.FieldProperty[]>({
-  key: `${PREFIX}copyableDstAppPropertiesState`,
-  get: async ({ get }) => {
-    const dstAppProperties = get(dstAppPropertiesState);
+export const copyableDstAppPropertiesState = atom<Promise<kintoneAPI.FieldProperty[]>>(
+  async (get) => {
+    const dstAppProperties = await get(dstAppPropertiesState);
     const filtered = filterFieldProperties(
       dstAppProperties,
       (field) =>
         field.type !== 'SUBTABLE' && field.type !== 'GROUP' && field.type !== 'REFERENCE_TABLE'
     );
     return Object.values(filtered).sort((a, b) => a.label.localeCompare(b.label, 'ja'));
-  },
-});
+  }
+);
 
-export const targetDstAppPropertiesState = selector<kintoneAPI.FieldProperty[]>({
-  key: `${PREFIX}targetDstAppPropertiesState`,
-  get: async ({ get }) => {
-    const dstAppProperties = get(dstAppPropertiesState);
+export const targetDstAppPropertiesState = atom<Promise<kintoneAPI.FieldProperty[]>>(
+  async (get) => {
+    const dstAppProperties = await get(dstAppPropertiesState);
     const filtered = filterFieldProperties(
       dstAppProperties,
       (field) => field.type === 'SINGLE_LINE_TEXT' || field.type === 'NUMBER'
     );
     return Object.values(filtered).sort((a, b) => a.label.localeCompare(b.label, 'ja'));
-  },
-});
+  }
+);
 
-export const dstAppSubtablePropertiesState = selector<kintoneAPI.property.Subtable[]>({
-  key: `${PREFIX}dstAppSubtablePropertiesState`,
-  get: async ({ get }) => {
-    const dstAppProperties = get(dstAppPropertiesState);
+export const dstAppSubtablePropertiesState = atom<Promise<kintoneAPI.property.Subtable[]>>(
+  async (get) => {
+    const dstAppProperties = await get(dstAppPropertiesState);
     const subtables = Object.values(dstAppProperties).filter((field) => field.type === 'SUBTABLE');
     return subtables;
-  },
-});
+  }
+);
 
-export const dstAppInsubtablePropertiesState = selector<kintoneAPI.property.InSubtable[]>({
-  key: `${PREFIX}dstAppInsubtablePropertiesState`,
-  get: async ({ get }) => {
-    const subtableProperties = get(dstAppSubtablePropertiesState);
-    const fieldCode = get(dstSubtableFieldCodeState);
-    if (!fieldCode) {
-      return [];
+export const srcAppPropertiesState = atom<Promise<kintoneAPI.FieldProperty[]>>(async (get) => {
+  const srcAppId = get(srcAppIdAtom);
+  if (!srcAppId) {
+    return [];
+  }
+
+  const allApps = await get(kintoneAppsAtom);
+  const srcApp = allApps.find((app) => app.appId === srcAppId);
+  if (!srcApp) {
+    return [];
+  }
+
+  let guestSpaceId;
+  if (srcApp.spaceId) {
+    const spaces = await get(kintoneSpacesAtom);
+    const space = spaces.find((s) => s.id === srcApp.spaceId);
+    if (space?.isGuest) {
+      guestSpaceId = space.id;
     }
+  }
 
-    const subtable = subtableProperties.find((field) => field.code === fieldCode);
-    if (!subtable) {
-      return [];
-    }
+  const { properties } = await getFormFields({
+    app: srcAppId,
+    preview: true,
+    guestSpaceId,
+    debug: !isProd,
+  });
+  const filtered = omitFieldProperties(properties, ['GROUP', 'SUBTABLE', 'REFERENCE_TABLE']);
 
-    const filtered = filterFieldProperties(
-      subtable.fields,
-      (field) => field.type === 'SINGLE_LINE_TEXT'
-    );
-    return Object.values(filtered).sort((a, b) =>
-      a.label.localeCompare(b.label, 'ja')
-    ) as kintoneAPI.property.InSubtable[];
-  },
-});
-
-export const srcAppPropertiesState = selector<kintoneAPI.FieldProperty[]>({
-  key: `${PREFIX}srcAppPropertiesState`,
-  get: async ({ get }) => {
-    const srcAppId = get(srcAppIdState);
-    if (!srcAppId) {
-      return [];
-    }
-
-    const allApps = get(kintoneAppsState);
-    const srcApp = allApps.find((app) => app.appId === srcAppId);
-    if (!srcApp) {
-      return [];
-    }
-
-    let guestSpaceId;
-    if (srcApp.spaceId) {
-      const spaces = get(kintoneSpacesState);
-      const space = spaces.find((s) => s.id === srcApp.spaceId);
-      if (space?.isGuest) {
-        guestSpaceId = space.id;
-      }
-    }
-
-    const { properties } = await getFormFields({
-      app: srcAppId,
-      preview: true,
-      guestSpaceId,
-      debug: ENV === 'development',
-    });
-    const filtered = omitFieldProperties(properties, ['GROUP', 'SUBTABLE', 'REFERENCE_TABLE']);
-
-    return Object.values(filtered).sort((a, b) => a.label.localeCompare(b.label, 'ja'));
-  },
+  return Object.values(filtered).sort((a, b) => a.label.localeCompare(b.label, 'ja'));
 });
--- src/config/states/plugin.ts
+++ src/config/states/plugin.ts
@@ -1,146 +1,87 @@
 import { restorePluginConfig } from '@/lib/plugin';
 import { PluginCondition, PluginConfig } from '@/schema/plugin-config';
 import { produce } from 'immer';
-import { DefaultValue, RecoilState, atom, selector, selectorFamily } from 'recoil';
+import { atom } from 'jotai';
+import { atomWithDefault } from 'jotai/utils';
+import { SetStateAction } from 'react';
 
-const PREFIX = 'plugin';
+export const pluginConfigAtom = atom<PluginConfig>(restorePluginConfig());
 
-export const storageState = atom<PluginConfig>({
-  key: `${PREFIX}storageState`,
-  default: restorePluginConfig(),
-});
+export const loadingState = atom<boolean>(false);
 
-export const loadingState = atom<boolean>({
-  key: `${PREFIX}loadingState`,
-  default: false,
-});
-
-export const conditionsState = selector<PluginCondition[]>({
-  key: `${PREFIX}conditionsState`,
-  get: ({ get }) => {
-    const storage = get(storageState);
-    return storage.conditions;
-  },
-  set: ({ set }, newValue) => {
-    if (newValue instanceof DefaultValue) {
-      return;
-    }
-    set(storageState, (current) =>
+export const pluginConditionsAtom = atom(
+  (get) => get(pluginConfigAtom).conditions,
+  (_, set, newValue: SetStateAction<PluginCondition[]>) => {
+    set(pluginConfigAtom, (current) =>
       produce(current, (draft) => {
-        draft.conditions = newValue;
+        draft.conditions = typeof newValue === 'function' ? newValue(draft.conditions) : newValue;
       })
     );
-  },
+  }
+);
+
+export const selectedConditionIdAtom = atomWithDefault<string>((get) => {
+  const config = get(pluginConfigAtom);
+  return config.conditions[0].id;
 });
 
-export const selectedConditionIdState = atom<string | null>({
-  key: `${PREFIX}selectedConditionIdState`,
-  default: selector<string | null>({
-    key: `${PREFIX}selectedConditionIdStateDefault`,
-    get: ({ get }) => {
-      return get(conditionsState)[0]?.id ?? null;
-    },
-  }),
-});
-
-export const selectedConditionState = selector<PluginCondition>({
-  key: `${PREFIX}selectedConditionState`,
-  get: ({ get }) => {
-    const conditions = get(conditionsState);
-    const selectedId = get(selectedConditionIdState);
-    return conditions.find((condition) => condition.id === selectedId) ?? conditions[0]!;
+export const selectedConditionAtom = atom(
+  (get) => {
+    const conditions = get(pluginConditionsAtom);
+    const selectedConditionId = get(selectedConditionIdAtom);
+    return conditions.find((condition) => condition.id === selectedConditionId) ?? conditions[0]!;
   },
-  set: ({ get, set }, newValue) => {
-    if (newValue instanceof DefaultValue) {
+  (get, set, newValue: SetStateAction<PluginCondition>) => {
+    const selectedConditionId = get(selectedConditionIdAtom);
+    const conditions = get(pluginConditionsAtom);
+    const index = conditions.findIndex((condition) => condition.id === selectedConditionId);
+    if (index === -1) {
       return;
     }
-    const conditions = get(conditionsState);
-    const index = conditions.findIndex((condition) => condition.id === newValue.id);
-    set(conditionsState, conditions.toSpliced(index, 1, newValue));
-  },
-});
+    set(pluginConfigAtom, (current) =>
+      produce(current, (draft) => {
+        draft.conditions[index] =
+          typeof newValue === 'function' ? newValue(draft.conditions[index]) : newValue;
+      })
+    );
+  }
+);
 
-const conditionPropertyState = selectorFamily<
-  PluginCondition[keyof PluginCondition],
-  keyof PluginCondition
->({
-  key: `${PREFIX}conditionPropertyState`,
-  get:
-    (key) =>
-    ({ get }) => {
-      const selectedCondition = get(selectedConditionState);
-      return selectedCondition[key];
+export const getConditionPropertyAtom = <T extends keyof PluginCondition>(property: T) =>
+  atom(
+    (get) => {
+      return get(selectedConditionAtom)[property];
     },
-  set:
-    (key) =>
-    ({ get, set }, newValue) => {
-      if (newValue instanceof DefaultValue) {
-        process.env.NODE_ENV === 'development' && console.warn('newValue is DefaultValue');
-        return;
-      }
-      set(selectedConditionState, (current) =>
-        produce(current, (draft) => {
-          // @ts-ignore
-          draft[key] = newValue;
+    (_, set, newValue: SetStateAction<PluginCondition[T]>) => {
+      set(selectedConditionAtom, (condition) =>
+        produce(condition, (draft) => {
+          draft[property] = typeof newValue === 'function' ? newValue(draft[property]) : newValue;
         })
       );
-    },
-});
+    }
+  );
 
-export const getConditionPropertyState = <T extends keyof PluginCondition>(property: T) =>
-  conditionPropertyState(property) as unknown as RecoilState<PluginCondition[T]>;
-
-export const dstFieldState = conditionPropertyState('dstField') as RecoilState<
-  PluginCondition['dstField']
->;
-export const srcAppIdState = conditionPropertyState('srcAppId') as RecoilState<
-  PluginCondition['srcAppId']
->;
-export const srcSpaceIdState = conditionPropertyState('srcSpaceId') as RecoilState<
-  PluginCondition['srcSpaceId']
->;
-export const isSrcAppGuestSpaceState = conditionPropertyState('isSrcAppGuestSpace') as RecoilState<
-  PluginCondition['isSrcAppGuestSpace']
->;
-export const srcFieldState = conditionPropertyState('srcField') as RecoilState<
-  PluginCondition['srcField']
->;
-export const copiesState = conditionPropertyState('copies') as RecoilState<
-  PluginCondition['copies']
->;
-export const displayFieldsState = conditionPropertyState('displayFields') as RecoilState<
-  PluginCondition['displayFields']
->;
-export const queryState = conditionPropertyState('filterQuery') as RecoilState<
-  PluginCondition['filterQuery']
->;
-export const enablesCacheState = conditionPropertyState('enablesCache') as RecoilState<
-  PluginCondition['enablesCache']
->;
-export const autoLookupState = conditionPropertyState('autoLookup') as RecoilState<
-  PluginCondition['autoLookup']
->;
-export const enablesValidationState = conditionPropertyState('enablesValidation') as RecoilState<
-  PluginCondition['enablesValidation']
->;
-export const saveAndLookupState = conditionPropertyState('saveAndLookup') as RecoilState<
-  PluginCondition['saveAndLookup']
->;
-export const isCaseSensitiveState = conditionPropertyState('isCaseSensitive') as RecoilState<
-  PluginCondition['isCaseSensitive']
->;
-export const isKatakanaSensitiveState = conditionPropertyState(
-  'isKatakanaSensitive'
-) as RecoilState<PluginCondition['isKatakanaSensitive']>;
-export const isHankakuKatakanaSensitiveState = conditionPropertyState(
+export const dstFieldAtom = getConditionPropertyAtom('dstField');
+export const srcAppIdAtom = getConditionPropertyAtom('srcAppId');
+export const srcSpaceIdAtom = getConditionPropertyAtom('srcSpaceId');
+export const isSrcAppGuestSpaceAtom = getConditionPropertyAtom('isSrcAppGuestSpace');
+export const srcFieldAtom = getConditionPropertyAtom('srcField');
+export const copiesAtom = getConditionPropertyAtom('copies');
+export const displayFieldsAtom = getConditionPropertyAtom('displayFields');
+export const queryAtom = getConditionPropertyAtom('filterQuery');
+export const enablesCacheAtom = getConditionPropertyAtom('enablesCache');
+export const autoLookupAtom = getConditionPropertyAtom('autoLookup');
+export const enablesValidationAtom = getConditionPropertyAtom('enablesValidation');
+export const saveAndLookupAtom = getConditionPropertyAtom('saveAndLookup');
+export const isCaseSensitiveAtom = getConditionPropertyAtom('isCaseSensitive');
+export const isKatakanaSensitiveAtom = getConditionPropertyAtom('isKatakanaSensitive');
+export const isHankakuKatakanaSensitiveAtom = getConditionPropertyAtom(
   'isHankakuKatakanaSensitive'
-) as RecoilState<PluginCondition['isHankakuKatakanaSensitive']>;
-export const isZenkakuEisujiSensitiveState = conditionPropertyState(
-  'isZenkakuEisujiSensitive'
-) as RecoilState<PluginCondition['isZenkakuEisujiSensitive']>;
-export const conditionTypeState = getConditionPropertyState('type');
-export const dstSubtableFieldCodeState = getConditionPropertyState('dstSubtableFieldCode');
-export const dstInsubtableFieldCodeState = getConditionPropertyState('dstInsubtableFieldCode');
-export const insubtableCopiesState = getConditionPropertyState('insubtableCopies');
-export const isFailSoftEnabledState = getConditionPropertyState('isFailSoftEnabled');
-export const sortCriteriaState = getConditionPropertyState('sortCriteria');
+);
+export const isZenkakuEisujiSensitiveAtom = getConditionPropertyAtom('isZenkakuEisujiSensitive');
+export const conditionTypeAtom = getConditionPropertyAtom('type');
+export const dstSubtableFieldCodeAtom = getConditionPropertyAtom('dstSubtableFieldCode');
+export const dstInsubtableFieldCodeAtom = getConditionPropertyAtom('dstInsubtableFieldCode');
+export const insubtableCopiesAtom = getConditionPropertyAtom('insubtableCopies');
+export const isFailSoftEnabledAtom = getConditionPropertyAtom('isFailSoftEnabled');
+export const sortCriteriaAtom = getConditionPropertyAtom('sortCriteria');
