acb67f3675eaea14b13fa7f85db06392844d76a4
Ribbit <echo07200503@gmail.com>
2025-01-26 10:37:27 +0900

    ♻ DialogTitleコンポーネントをSuspenseでラップし、データ取得中のフォールバックを追加

--- src/desktop/embedding-lookup-button/components/dialog/title.tsx
+++ src/desktop/embedding-lookup-button/components/dialog/title.tsx
@@ -1,13 +1,21 @@
 import { DialogTitle } from '@mui/material';
 import { useAtomValue } from 'jotai';
-import { FC } from 'react';
+import { FC, Suspense } from 'react';
 import { dialogTitleAtom } from '../../states/dialog';
 import { useConditionId } from '../attachment-context';
 
-const Container: FC = () => {
+const DialogTitleComponent: FC = () => {
   const conditionId = useConditionId();
   const title = useAtomValue(dialogTitleAtom(conditionId));
   return <DialogTitle>{title}</DialogTitle>;
 };
 
-export default Container;
+const DialogTitleContainer: FC = () => {
+  return (
+    <Suspense fallback={<DialogTitle>アプリからデータを取得</DialogTitle>}>
+      <DialogTitleComponent />
+    </Suspense>
+  );
+};
+
+export default DialogTitleContainer;
