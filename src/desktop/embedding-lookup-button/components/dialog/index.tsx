60e44946174b1155a80a672acd425bd408419807
Ribbit <echo07200503@gmail.com>
2025-02-01 14:18:26 +0900

    ✨ ダイアログコンポーネントに行のフォーカス管理機能を追加

--- src/desktop/embedding-lookup-button/components/dialog/index.tsx
+++ src/desktop/embedding-lookup-button/components/dialog/index.tsx
@@ -2,19 +2,21 @@ import { PluginErrorBoundary } from '@/lib/components/error-boundary';
 import { t } from '@/lib/i18n';
 import styled from '@emotion/styled';
 import { Button, Dialog, DialogActions, DialogContent } from '@mui/material';
-import { useAtom } from 'jotai';
+import { useAtom, useSetAtom } from 'jotai';
+import { RESET } from 'jotai/utils';
 import { FC, FCX, useEffect, useRef } from 'react';
 import FailSoftAlert from '../../fail-soft-alert';
-import { isDialogShownAtom } from '../../states/dialog';
+import { focusedRowIndexAtom, isDialogShownAtom } from '../../states/dialog';
 import { useAttachmentProps } from '../attachment-context';
 import Header from './header';
+import DialogLoading from './loading';
 import Table from './table';
 import Title from './title';
-import DialogLoading from './loading';
 
 const DialogComponent: FCX = ({ className }) => {
   const searchInputRef = useRef<HTMLInputElement>(null);
   const attachmentProps = useAttachmentProps();
+  const setFocusedRowIndex = useSetAtom(focusedRowIndexAtom(attachmentProps));
   const [open, setOpen] = useAtom(isDialogShownAtom(attachmentProps));
 
   const onClose = () => setOpen(false);
@@ -22,6 +24,7 @@ const DialogComponent: FCX = ({ className }) => {
   useEffect(() => {
     // ダイアログを開いたときに検索入力欄にフォーカスを当てる
     if (open) {
+      setFocusedRowIndex(RESET);
       const focusInput = () => {
         searchInputRef.current?.focus();
       };
